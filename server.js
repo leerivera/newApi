efb5a79c71e1453bb002206b3f61936048d91dd7
leerivera <leerivera80@gmail.com>
2022-06-25 10:08:35 -0400

    get

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+
+const PORT = 8000
+
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.getMaxListeners('/api', (request, response) => {
+     response.json()
+})
+
+
+app.listen(PORT, () => {
+    console.log(`the server is running on ${PORT}! damn that's fast`)
+})
